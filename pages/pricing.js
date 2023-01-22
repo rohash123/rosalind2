db3336b99e85fdf799a54159b066c3ead580fd2e
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2023-01-22 14:47:38 -0800

    pricing fix

--- pages/pricing.js
+++ pages/pricing.js
@@ -276,38 +276,7 @@ export default function Pricing() {
             </div>
           ))}
           </div>
-        <h2 className="text-lg mt-8 font-medium leading-6 text-gray-900">Text Completion</h2>
-        <div className="mt-12 space-y-4 sm:mt-8 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
-          {language.map((image) => (
-            <div key={image.name} className="divide-y divide-gray-200 rounded-lg border border-gray-200 shadow-sm">
-              <div className="p-6">
-                <h2 className="text-lg font-medium leading-6 text-gray-900">{image.name}</h2>
-                <p className="mt-4 text-sm text-gray-500">{image.description}</p>
-                <p className="mt-8">
-                  <span className="text-xl font-bold tracking-tight text-gray-900">${image.priceMonthly}</span>{' '}
-                  <span className="text-base font-medium text-gray-500">/1K tokens</span>
-                </p>
-                <a
-                  href={image.href}
-                  className="mt-8 block w-full rounded-md border border-gray-800 bg-gray-800 py-2 text-center text-sm font-semibold text-white hover:bg-gray-900"
-                >
-                  View in Docs
-                </a>
-              </div>
-              <div className="px-6 pt-6 pb-8">
-                <h3 className="text-sm font-medium text-gray-900">What's included</h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {image.includedFeatures.map((feature) => (
-                    <li key={feature} className="flex space-x-3">
-                      <CheckIcon className="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
-                      <span className="text-sm text-gray-500">{feature}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-          </div>
+        
         <h2 className="text-lg mt-8 font-medium leading-6 text-gray-900">Image Generation</h2>
         <div className="mt-12 space-y-4 sm:mt-8 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
           {image.map((language) => (
