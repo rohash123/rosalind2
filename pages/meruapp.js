56b02fa9f77c8fb383c2b8e57114f391c7fb2db3
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2023-02-02 21:30:17 -0800

    trying

--- pages/meruapp.js
+++ pages/meruapp.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Fragment, useState, useEffect } from "react";
 import { Dialog, Transition } from '@headlessui/react'
-import {API, Hub, Amplify, withServerContext, withSSRContext } from 'aws-amplify'
+import {API, Auth, Hub, Amplify, withServerContext, withSSRContext } from 'aws-amplify'
 import AuthComponent from "../components/AuthComponent";
 import Banner from "../components/Banner"
 import awsExports from '../src/aws-exports'
@@ -130,6 +130,9 @@ export default function MeruApp({token}){
     const [resetDisabled, setResetDisabled] = useState(false)
     const [query, setQuery] = useState(false)
     const [code, setCode] = useState('')
+    const refreshData = () => {
+        router.replace(router.asPath);
+      };
 
     Hub.listen('auth', (data) => {
         switch (data.payload.event) {
@@ -138,9 +141,7 @@ export default function MeruApp({token}){
               refreshData()
         }
     })
-    const refreshData = () => {
-        router.replace(router.asPath);
-      };
+    
     async function getkey(){
         let sub = user['attributes']['sub']
         let requestOptions = {
@@ -155,10 +156,6 @@ export default function MeruApp({token}){
         setApiKey(data.apikey)
    }
    async function resetkey(event){
-    if (resetDisabled == true){
-        return
-    }
-    else{
         setResetDisabled(true)
         console.log('hi')
         let sub = user['attributes']['sub']
@@ -172,8 +169,6 @@ export default function MeruApp({token}){
         await response.json()
         setResetDisabled(false)
         refreshData()
-    }
-    
 }
 async function createkey(event){
     console.log('hi')
@@ -724,9 +719,18 @@ async function addtoDB(f,state,response){
                     <button onClick={()=>setShow(true)} className="font-medium text-pink-400 hover:text-pink-600">
                       Show
                     </button>
-                    <button onClick={resetkey} disabled={resetDisabled} className="ml-3 font-medium text-red-600 hover:text-red-600">
+                    {!resetDisabled && (<button onClick={resetkey} className="ml-3 font-medium text-red-600 hover:text-red-600">
                       Reset
-                    </button>
+                    </button>)}
+                    {resetDisabled && (<div class="text-right">
+    <div role="status">
+        <svg aria-hidden="true" class="w-4 h-4 text-gray-200 animate-spin dark:text-gray-100 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>)}
                   </div>
                 </li>)}
                 {show && (<li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
@@ -737,9 +741,18 @@ async function addtoDB(f,state,response){
                     <button onClick={()=>setShow(false)} className="font-medium text-pink-400 hover:text-pink-600">
                       Hide
                     </button>
-                    <button onClick={resetkey} className="ml-3 font-medium text-red-600 hover:text-red-600">
+                    {!resetDisabled && (<button onClick={resetkey} className="ml-3 font-medium text-red-600 hover:text-red-600">
                       Reset
-                    </button>
+                    </button>)}
+                    {resetDisabled && (<div class="text-right">
+    <div role="status">
+        <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-100 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>)}
                   </div>
                 </li>)}
                 
