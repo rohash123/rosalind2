d70110f1cf3dcd23acf7002c9b6a1fe40548a9c6
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2023-02-03 18:22:49 -0800

    dropbox

--- pages/meruapp.js
+++ pages/meruapp.js
@@ -618,13 +618,12 @@ async function addtoDB(f,state,response){
           </div>
           {/* Indexed Documents */}
           {(active == 'Indexes') &&(<>
-            <div className="mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8">
           {/* Left sidebar & main wrapper */}
-          <div className="min-w-0 flex-1 bg-white xl:flex">
-            <div className="border-b border-gray-200 bg-white xl:w-64 xl:flex-shrink-0 xl:border-b-0 xl:border-r xl:border-gray-200">
+          <div className="min-w-0 px-5 flex-1 bg-white xl:flex overflow-y-auto">
+            <div className="border-b border-gray-200 bg-white xl:w-96 xl:flex-shrink-0 xl:border-b-0 xl:border-r xl:border-gray-200">
               <div className="overflow-auto h-full py-6 pl-4 pr-6 sm:pl-6 lg:pl-8 xl:pl-0">
                 {/* Start left column area */}
-                <div className="relative h-20 xl:h-full l:h-full overflow-auto" >
+                <div className="relative h-20 xl:h-full l:h-full" >
                 {/* <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8"> */}
                 {!fileList &&( <p>Loading Your Indexes...</p>)}
                 {fileList && (
@@ -656,8 +655,8 @@ async function addtoDB(f,state,response){
               </div>
               </div>
             </div>
-
-            <div className=" overflow-auto bg-white lg:min-w-0 lg:flex-1">
+            <main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+              {/* Start main area*/}
               <div className="overflow-auto h-full py-6 px-4 sm:px-6 lg:px-8">
                 {/* Start main area*/}
                 <div className="relative h-full">
@@ -670,11 +669,12 @@ async function addtoDB(f,state,response){
               </div>)
                
               }
+              
                 </div>
                 {/* End main area */}
               </div>
-            </div>
-          </div>
+              {/* End main area */}
+            </main>
           
           
           </div></>)}
@@ -709,7 +709,7 @@ async function addtoDB(f,state,response){
                 <div>
                   {!done && <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-gray-100">
                   <div role="status">
-        <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-100 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-100 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="https://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
         </svg>
@@ -718,7 +718,7 @@ async function addtoDB(f,state,response){
                   </div>}
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                      {done || 'Your Index is being queued. Please wait one moment.'}
+                      {done || 'Your Index is being queued. Please wait one moment. If this persists for longer, please contact support.'}
                     </Dialog.Title>
                     <div className="mt-2">
                     </div>
@@ -749,7 +749,7 @@ async function addtoDB(f,state,response){
                 extensions={['.pdf','.txt']} >
                 <div className=" dropbox-button w-60 cursor-pointer inline-flex items-center rounded border border-transparent bg-pink-400 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2">{!dbfiles && ('Add Files')}{dbfiles && ('Replace Files')}</div> 
             </DropboxChooser>)}
-            {!integrations.dropbox.accessToken && (<div className=" dropbox-button w-full cursor-pointer inline-flex items-center rounded border border-transparent bg-pink-400 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2">Please Connect Your Dropox Account in 'Account' or use the API</div>)}
+            {!integrations.dropbox.accessToken && (<div className=" dropbox-button w-80 cursor-pointer inline-flex items-center rounded border border-pink-400 bg-transparent px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2">Please Connect Your Dropox Account in 'Account' or use the API</div>)}
             
             {/* {!dbfiles[0] &&( <p>Select a file to begin</p>)} */}
                 {dbfiles && (
@@ -783,7 +783,7 @@ async function addtoDB(f,state,response){
                 type="text"
                 name="indexname"
                 id="indexname"
-                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="block w-full rounded-md bg-transparent border-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 onChange = {e => setIndexName(e.target.value)}
                 value = {indexName}
                 placeholder="Jerry Seinfield"
@@ -820,7 +820,7 @@ async function addtoDB(f,state,response){
             <QueryHistory apikey={apiKey}/>
         )}
           {/* Account Details */}
-         {(active == 'Account') &&(<div className="relative z-0 flex flex-1 overflow-hidden">
+         {(active == 'Account') &&(<div className="relative z-0 flex flex-1 overflow-y-auto">
             <main className="relative z-0 flex-1 focus:outline-none xl:order-last">
               {/* Start main area*/}
               <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
