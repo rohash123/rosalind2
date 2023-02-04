3279587d751656f47505bceb9388b9d8e3ecadd6
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2023-02-03 17:31:09 -0800

    discord2

--- pages/index.js
+++ pages/index.js
@@ -88,7 +88,7 @@ export default function Example() {
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   <span className="text-gray-600">
-                    Explore our Community and get help{' '}
+                    Explore our community and get help when you need it.{' '}
                     <a href="https://t.co/ujM1RAVKZE" className="font-semibold text-pink-400">
                       <span className="absolute inset-0" aria-hidden="true" />
                       Join the Discord <span aria-hidden="true">&rarr;</span>
