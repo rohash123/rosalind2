43d8982d6989eae883bec3b62d0b74d3d75a6c32
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2023-02-06 02:06:06 -0800

    testing

--- pages/meruapp.js
+++ pages/meruapp.js
@@ -293,15 +293,17 @@ async function addtoDB(f,state,response){
     }
     useEffect(() => {
         async function createUser(){
+            if(user){
+                return    
+            }
             const response = await Auth.currentAuthenticatedUser()
             setUser(response)
             setLoggedIn(true)
-            console.log(response)
-            console.log(window.location)
             try{
                 let authcode = window.location.search.slice(1).split("&")[0].split("=")[1]
                 let state = window.location.search.slice(1).split("&")[1].split('=')[1]
                 if(state != ''){
+                    console.log(state)
                     if(state == 'dropbox'){
                         const config = {
                             clientId: 'rqiucchpvi1uywj',
